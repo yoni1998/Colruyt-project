302cc2b74a924a6117ba55ae9ec5e54ecc3ab57d
YoniVi <yoni.vindelinckx@inetum-realdolmen.world>
2023-04-04 16:39:18 +0200

    update dotenv

--- graphqlGateway/datasource/userDatasource.ts
+++ graphqlGateway/datasource/userDatasource.ts
@@ -1,14 +1,17 @@
 import { RESTDataSource } from "apollo-datasource-rest";
-
+import * as dotenv from "dotenv";
+dotenv.config();
 export class UserDatasource extends RESTDataSource {
+  ROOTPATH: string = process.env.ROOTPATH as string;
+  SUBPATH: string = process.env.SUBPATH as string;
   constructor() {
     super();
   }
   async getAllPersons() {
-    return await this.get(`http://localhost:7000/api/users`);
+    return await this.get(this.ROOTPATH);
   }
   async getPerson(id: any) {
-    return await this.get(`http://localhost:7000/api/user/${id}`);
+    return await this.get(this.SUBPATH + id);
   }
   async deletePerson(id: any) {
     let options = {
@@ -17,20 +20,20 @@ export class UserDatasource extends RESTDataSource {
       cache: "no-cache",
       credentials: "omit",
     };
-    return await this.delete(`http://localhost:7000/api/user/${id}`, options);
+    return await this.delete(this.SUBPATH + id, options);
   }
   async addPerson(person: any) {
     const options = {
       method: "post",
       headers: { "Content-Type": "application/json" },
     };
-    return this.post(`http://localhost:7000/api/user`, person, options);
+    return this.post(this.SUBPATH, person, options);
   }
   async updatePerson(id: any, person: any) {
     const options = {
       method: "put",
       headers: { "Content-Type": "application/json" },
     };
-    return this.put(`http://localhost:7000/api/user/${id}`, person, options);
+    return this.put(this.SUBPATH + id, person, options);
   }
 }
